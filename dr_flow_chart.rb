368670f4dcb5e661c2bf5eecc399fccacfb9665d
John Erwin Chan <jcsserv@gmail.com>
2021-04-13 09:56:07 +0800

    updated main script

--- dr_flow_chart.rb
+++ dr_flow_chart.rb
@@ -1,18 +1,54 @@
 class DrFlowChart
+
+    attr_accessor :temp_flag, :full_flag
+
+    def initialize
+        puts "Turning ON HOT/OLD Taps"
+    end
   
     def turn_on_taps
+        puts "Turn ON/Adjust the HOT/COLD Taps"
+        puts "Is it Too Hot/Cold [Y,N]"
+        @temp_flag=gets.chomp
+        check_if_too_hot_cold(@temp_flag)
     end
 
-    def check_if_too_hot_cold
+    def check_if_too_hot_cold(flag)     
+        if not ["y", "n"].include?(flag.downcase) then
+            puts "Only enter  [Y, N]"
+            @temp_flag=gets.chomp
+            check_if_too_hot_cold(@temp_flag)
+        elsif flag.downcase== "y" then
+            turn_on_taps
+        elsif flag.downcase=='n' then
+            wait_for_2_minutes
+        end
+
     end
 
     def wait_for_2_minutes
+        puts "PLease wait for 2 minutes"
+        sleep(120)
+        puts "Is it Bath Full [Y,N]"
+        @full_flag=gets.chomp
+        check_if_bath_full(@full_flag)
     end
 
-    def check_if_bath_full
+    def check_if_bath_full(flag)
+        
+        if not ["y", "n"].include?(flag.downcase) then
+            puts "Only enter  [Y, N]"
+            @full_flag=gets.chomp
+            check_if_bath_full(@full_flag)
+        elsif flag.downcase=="y" then
+            turn_off_taps
+        elsif flag.downcase=='n' then
+            wait_for_2_minutes
+        end
     end
     
     def turn_off_taps
+        puts "TURNING OFF TAPS -- THANK YOU"
     end
 
 end
