631c9400b860a99b7eca8bbade7a3fb2a14f090d
John Erwin Chan <jcsserv@gmail.com>
2021-04-13 09:18:50 +0800

    update bare

---
+++ dr_flow_chart.rb
@@ -0,0 +1,18 @@
+class DrFlowChart
+  
+    def turn_on_taps
+    end
+
+    def check_if_too_hot_cold
+    end
+
+    def wait_for_2_minutes
+    end
+
+    def check_if_bath_full
+    end
+    
+    def turn_off_taps
+    end
+
+end
